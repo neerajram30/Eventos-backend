4702a71896df2162cc537d8566fe5fd82cd8ea9b
neerajram30 <neerajramachandran30@gmail.com>
2022-07-12 23:33:01 +0530

    User authentication created

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,9 +1,121 @@
-const express =require('express');
+const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const User = require("../../models/User")
+const auth = require("../../middlewares/auth")
 
-router.get("/login",(req,res)=>{
-    console.log('requested');
-})
+router.post(
+    "/login",
+    [
+        check("email", "Please enter a valid email").isEmail(),
+        check("password", "Please enter a valid password").isLength({
+            min: 6
+        })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+        const { email, password } = req.body;
+        try {
+            let user = await User.findOne({
+                email
+            });
+            if (!user)
+                return res.status(400).json({
+                    message: "User Not Exist"
+                });
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch)
+                return res.status(400).json({
+                    message: "Incorrect Password !"
+                });
+            if (user && isMatch) {
+                req.session.loggedIn = true
+                req.session.name = user.username
+                console.log(req.session);
+                return res.status(200).json({
+                    message: "login success"
+                });
+            }
+        } catch (e) {
+            console.error(e);
+            res.status(500).json({
+                message: "Server Error"
+            });
+        }
+    }
+);
 
+router.post(
+    "/register",
+    [
+        check("username", "Please Enter a Valid Username")
+            .not()
+            .isEmpty(),
+        check("email", "Please enter a valid email").isEmail(),
+        check("password", "Please enter a valid password").isLength({
+            min: 6
+        })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        const {
+            username,
+            email,
+            password
+        } = req.body;
+        try {
+            let user = await User.findOne({
+                email
+            });
+            if (user) {
+                return res.status(400).json({
+                    msg: "User Already Exists"
+                });
+            }
+
+            user = new User({
+                username,
+                email,
+                password
+            });
+
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+            await user.save();
+            res.status(200).send("Registered")
+        } catch (err) {
+            console.log(err.message);
+            res.status(500).send("Error in Saving");
+        }
+    }
+);
+
+
+router.get("/me", auth, async (req, res) => {
+    res.json({ message: "logged in" });
+    console.log(req.session);
+});
+
+router.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            return console.log(err);
+        }
+        res.json({ message: "logged out" });
+
+    });
+});
 
 module.exports = router;
