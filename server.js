2716d256a3297793159784d746898415d10a470c
neerajram30 <neerajramachandran30@gmail.com>
2022-07-13 21:27:19 +0530

    comments added

--- server.js
+++ server.js
@@ -4,22 +4,23 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const MongoDBSession=require('connect-mongodb-session')(session)
 const app = express();
+const user = require('./routes/api/user')
+const events = require('./routes/api/events')
 require('dotenv').config();
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
-
-const user = require('./routes/api/user')
-// const events = require('./routes/api/events')
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Set up mongoDB store for storing sessions
 const store = new MongoDBSession({
   uri:MONGO_URI,
   collection:"sessions"
 })
 
+//Set up session
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
@@ -27,10 +28,12 @@ app.use(session({
   store : store
 }))
 
+//Express connection
 app.listen(PORT,()=>{
     console.log("Connected on port "+PORT);
 })
 
+//Database connection
 mongoose
   .connect(
     MONGO_URI,
@@ -39,11 +42,11 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log("mongodb wrror"+err));
 
+//End point test
 app.get("/", (req, res) => {
     res.json({ message: "API Working" });
   });  
 
-  
 //Routes
 app.use("/api/user",user)
-// app.use("/api/events",events)
+app.use("/api/events",events)
