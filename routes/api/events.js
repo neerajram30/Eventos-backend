dc4980accbcbd2d5c3d9f92b8609f382b551a79f
neerajram30 <neerajramachandran30@gmail.com>
2022-07-15 01:02:21 +0530

    upate feature added

--- routes/api/events.js
+++ routes/api/events.js
@@ -61,4 +61,56 @@ router.get("/myevent", auth, (req, res) => {
     }).catch(err => console.log(err))
 })
 
+//Delete events by id
+router.delete("/:id", auth, (req, res) => {
+    Events.deleteOne({ _id: req.params.id, creator: req.session.user_id }).then((response) => {
+        console.log(response)
+        if (response.n > 0) {
+            res.status(200).json({ message: "sucessfully deteleted event" })
+        }
+        else {
+            res.status(404).json({ message: "some error occured while deleting" })
+        }
+    })
+})
+
+//List events by id
+router.get("/:id", (req, res) => {
+    Events.findById(req.params.id).then(event => {
+        if (event) {
+            res.status(200).json(event);
+        } else {
+            res.status(404).json({ message: "Event not found!" });
+        }
+    });
+});
+
+//Update event
+router.put("/:id", auth, async (req, res) => {
+    const { createdAt, title, startDate, endDate, venue, description
+    } = req.body
+    let event = new Events({
+        _id: req.params.id,
+        title,
+        startDate,
+        endDate,
+        venue,
+        description,
+        createdAt,
+        creator: req.session.user_id
+    })
+    console.log(event);
+    await Events.updateOne({ _id: req.params.id, creator: req.session.user_id }, event
+    ).then((response) => {
+        console.log(response)
+        if (response) {
+            res.status(200).json({ message: "Update successful!" });
+        }
+        else {
+            res.status(500).json({ message: "Error Upating Event" });
+        }
+    })
+
+})
+
 module.exports = router;
